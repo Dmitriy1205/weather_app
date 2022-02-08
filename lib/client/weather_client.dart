b658ec498c82aae9678c4062378bc19776a4ba81
dmytroplashkin <Dmitriycustom33>
2022-02-08 14:33:36 +0200

    added and connected Dio package

--- lib/client/weather_client.dart
+++ lib/client/weather_client.dart
@@ -1,17 +1,22 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:weather_app/model/weather_model.dart';
-import 'package:http/http.dart' as http;
 
 class WeatherClient {
-  Future<Weather> getCurrentWeather(double lat, double long) async {
-    final response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$long&exclude=hourly,daily&appid=f74b1847f8772c97edbb6f55db16e611&units=metric'));
-    final body = jsonDecode(response.body);
-    print(body);
-    return Weather.fromJson(body);
+  final Dio _dio = Dio();
+
+  Future<Weather?> getCurrentWeather(double lat, double long) async {
+    Weather? weather;
+    try {
+      Response response = await _dio.get(
+          'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$long&exclude=hourly,daily&appid=f74b1847f8772c97edbb6f55db16e611&units=metric');
+      // final body = jsonDecode(response.data);
+      print(response.data);
+      weather = Weather.fromJson(response.data);
+    } on DioError catch (e) {
+      print('DioError:$e');
+    }
+    return weather;
 
     //TODO on Exeption  if statusCode.......
   }
-
 }
